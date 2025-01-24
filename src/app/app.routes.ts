2c843bf2f938e1b14eb92b2632554342cdad962b
graua-git <graua@oregonstate.edu>
2025-01-23 22:41:49 -0500

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ContactMeComponent } from './pages/contact-me/contact-me.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'experience', component: ExperienceComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'contact-me', component: ContactMeComponent },
+];
