5ab5b35d3bbde3a1354306d0dbda606ee58a61dc
graua-git <graua@oregonstate.edu>
2025-02-25 16:00:51 -0500

    fixed footer links and some styling

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
 }
